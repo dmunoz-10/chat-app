6c15f66754a739e1bc361e90286c0d7e74a0c9d5
Daniel Muñoz <djmv225@gmail.com>
2024-06-09 00:24:47 -0500

    Creates messages in real-time

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
   post 'add/user', to: 'rooms#add_user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
